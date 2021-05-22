13050be01b37dd78f09df39a5942fa67bc2fe5de
Matteo <udemy.frioni@gmail.com>
2021-05-22 14:03:00 +0200

    create user and test

---
+++ app/core/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fieds):
+        """Create and saves new user"""
+        if not email:
+            raise ValueError("e-mail required")
+        user = self.model(email=self.normalize_email(email), **extra_fieds)
+        user.set_password(password)
+        user.save(self.db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports mail instead of name"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserManager()
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_mail(self):
+        """Test creation of new user with mail"""
+        email = 'test@mail.com'
+        password = 'password123'
+        user = get_user_model() \
+            .object \
+            .create_user(email=email, password=password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalized(self):
+        """test new user mail normalization"""
+        mail = 'user@MAIL.com'
+        user = get_user_model() \
+            .object \
+            .create_user(email=mail, password='password')
+        self.assertEqual(user.email, mail.lower())
+
+    def test_user_invalid_mail(self):
+        """creating user with no valid mail will raise error"""
+        with self.assertRaises(ValueError):
+            get_user_model().object.create_user(None,'pass')
