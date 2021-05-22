5497d0dab62123c3283526fb73ae7fc407f4f7c8
Matteo <udemy.frioni@gmail.com>
2021-05-22 17:50:54 +0200

    admin features

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,10 +1,24 @@
+from core import models
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-from core import models
+from django.utils.translation import gettext
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
-    list_display = ['email','name']
+    list_display = ['email', 'name']
+    fieldsets = ((None, {
+        'fields': ('url', 'title', 'content', 'sites')
+    }),
+                 ('Advanced options', {
+                     'classes': ('collapse',),
+                     'fields': ('registration_required', 'template_name'),
+                 }),)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -19,3 +19,15 @@ class AdminSiteTest(TestCase):
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """test that user edit page work"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEquals(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """test that user page is created"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEquals(res.status_code, 200)
